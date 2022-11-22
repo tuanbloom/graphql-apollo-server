0adad09ae533fe72ea6e5a1d84d91326cc8914bd
Sam Curry <cuzzlor@users.noreply.github.com>
2022-11-22 16:36:21 +0800

    Upgrade to ApolloServer v4 (#18)

--- src/plugins/introspection-control-plugin.ts
+++ src/plugins/introspection-control-plugin.ts
@@ -1,15 +1,13 @@
+import { ApolloServerPlugin, GraphQLRequestContext, GraphQLRequestListener } from '@apollo/server'
 import { GraphQLContext, isIntrospectionQuery } from '@makerxstudio/graphql-core'
 import { isProduction } from '@makerxstudio/node-common'
-import { AuthenticationError } from 'apollo-server'
-import { ApolloServerPlugin, GraphQLRequestContext, GraphQLRequestListener } from 'apollo-server-plugin-base'
 
 export const introspectionControlPlugin: ApolloServerPlugin<GraphQLContext> = {
   requestDidStart: ({
     request: { query },
-    context,
+    contextValue: { user },
   }: GraphQLRequestContext<GraphQLContext>): Promise<GraphQLRequestListener<GraphQLContext> | void> => {
-    if (isProduction && !context.user && isIntrospectionQuery(query))
-      throw new AuthenticationError('Unauthenticated introspection is not supported')
+    if (isProduction && !user && isIntrospectionQuery(query)) throw new Error('Unauthenticated introspection is not supported')
     return Promise.resolve()
   },
 }
--- src/plugins/logging-plugin.ts
+++ src/plugins/logging-plugin.ts
@@ -1,14 +1,30 @@
+import { ApolloServerPlugin, GraphQLRequestContextWillSendResponse, GraphQLRequestListener } from '@apollo/server'
+import { GraphQLRequestContextWillSendSubsequentPayload } from '@apollo/server/dist/esm/externalTypes/requestPipeline'
 import { GraphQLContext, logGraphQLOperation } from '@makerxstudio/graphql-core'
-import { ApolloServerPlugin, GraphQLRequestContextWillSendResponse, GraphQLRequestListener } from 'apollo-server-plugin-base'
 
 export const loggingPlugin: ApolloServerPlugin<GraphQLContext> = {
-  requestDidStart: (): Promise<GraphQLRequestListener<GraphQLContext>> => {
+  requestDidStart: ({ contextValue: { started, logger } }): Promise<GraphQLRequestListener<GraphQLContext>> => {
     const responseListener: GraphQLRequestListener<GraphQLContext> = {
       willSendResponse({
-        response: { errors },
-        context: { started, logger },
+        response: { body },
         request: { query, variables, operationName },
       }: GraphQLRequestContextWillSendResponse<GraphQLContext>): Promise<void> {
+        const errors = body.kind === 'single' ? body.singleResult.errors : body.initialResult.errors
+        logGraphQLOperation({
+          started,
+          operationName,
+          query,
+          variables,
+          result: { errors },
+          logger,
+        })
+        return Promise.resolve()
+      },
+      willSendSubsequentPayload({
+        response: { body },
+        request: { query, variables, operationName },
+      }: GraphQLRequestContextWillSendSubsequentPayload<GraphQLContext>): Promise<void> {
+        const errors = body.kind === 'single' ? body.singleResult.errors : body.initialResult.errors
         logGraphQLOperation({
           started,
           operationName,
