6b4ee871847aea27d88855f291834aed15278fda
Adam Chidlow <achidlow@users.noreply.github.com>
2022-11-28 11:31:20 +0800

    Add the ability to suppress logging of GraphQL requests.

--- src/plugins/logging-plugin.ts
+++ src/plugins/logging-plugin.ts
@@ -1,41 +1,62 @@
+import { GraphQLFormattedError } from 'graphql'
 import { ApolloServerPlugin, GraphQLRequestContextWillSendResponse, GraphQLRequestListener } from '@apollo/server'
 import { GraphQLRequestContextWillSendSubsequentPayload } from '@apollo/server/dist/esm/externalTypes/requestPipeline'
 import { GraphQLContext, logGraphQLOperation } from '@makerxstudio/graphql-core'
 
-export const loggingPlugin: ApolloServerPlugin<GraphQLContext> = {
-  requestDidStart: ({ contextValue: { started, logger } }): Promise<GraphQLRequestListener<GraphQLContext>> => {
-    const responseListener: GraphQLRequestListener<GraphQLContext> = {
-      willSendResponse({
-        response: { body },
-        request: { query, variables, operationName },
-      }: GraphQLRequestContextWillSendResponse<GraphQLContext>): Promise<void> {
-        const errors = body.kind === 'single' ? body.singleResult.errors : body.initialResult.errors
-        logGraphQLOperation({
-          started,
-          operationName,
-          query,
-          variables,
-          result: { errors },
-          logger,
-        })
-        return Promise.resolve()
-      },
-      willSendSubsequentPayload({
-        response: { body },
-        request: { query, variables, operationName },
-      }: GraphQLRequestContextWillSendSubsequentPayload<GraphQLContext>): Promise<void> {
-        const errors = body.kind === 'single' ? body.singleResult.errors : body.initialResult.errors
-        logGraphQLOperation({
-          started,
-          operationName,
-          query,
-          variables,
-          result: { errors },
-          logger,
-        })
-        return Promise.resolve()
-      },
-    }
-    return Promise.resolve(responseListener)
-  },
+export interface GraphQLRequestInfo<TContext extends GraphQLContext> {
+  readonly requestContext: GraphQLRequestContextWillSendResponse<TContext>
+  readonly isSubsequentPayload: boolean
+  readonly formattedErrors?: ReadonlyArray<GraphQLFormattedError>
 }
+
+export interface LoggingPluginOptions<TContext extends GraphQLContext> {
+  shouldIgnore?: (request: GraphQLRequestInfo<TContext>) => boolean
+}
+
+export function createLoggingPlugin<TContext extends GraphQLContext>(
+  options: LoggingPluginOptions<TContext>
+): ApolloServerPlugin<TContext> {
+  return {
+    requestDidStart: ({ contextValue: { started, logger } }): Promise<GraphQLRequestListener<TContext>> => {
+      function logIfNotIgnore(ctx: GraphQLRequestContextWillSendResponse<TContext>, isSubsequentPayload: boolean) {
+        const errors = ctx.response.body.kind === 'single' ? ctx.response.body.singleResult.errors : ctx.response.body.initialResult.errors
+        if (
+          !(
+            options.shouldIgnore &&
+            options.shouldIgnore({
+              requestContext: ctx,
+              isSubsequentPayload,
+              formattedErrors: errors,
+            })
+          )
+        ) {
+          logGraphQLOperation({
+            started,
+            operationName: ctx.request.operationName,
+            query: ctx.request.query,
+            variables: ctx.request.variables,
+            result: { errors },
+            logger,
+          })
+        }
+      }
+
+      const responseListener: GraphQLRequestListener<TContext> = {
+        willSendResponse(ctx: GraphQLRequestContextWillSendResponse<TContext>): Promise<void> {
+          logIfNotIgnore(ctx, false)
+          return Promise.resolve()
+        },
+        willSendSubsequentPayload(ctx: GraphQLRequestContextWillSendSubsequentPayload<TContext>): Promise<void> {
+          logIfNotIgnore(ctx, false)
+          return Promise.resolve()
+        },
+      }
+      return Promise.resolve(responseListener)
+    },
+  }
+}
+
+/**
+ * @deprecated use createLoggingPlugin() directly instead
+ */
+export const loggingPlugin: ApolloServerPlugin<GraphQLContext> = createLoggingPlugin({})
