96370231dbdc12b450c89f24baa46741642af85f
TuanNT <tuan@wearebloom.com.au>
2023-07-28 12:02:32 +0700

    Update owner

--- src/plugins/logging-plugin.ts
+++ src/plugins/logging-plugin.ts
@@ -1,8 +1,8 @@
 import { GraphQLFormattedError } from 'graphql'
 import { ApolloServerPlugin, GraphQLRequestContextWillSendResponse, GraphQLRequestListener } from '@apollo/server'
 import { GraphQLRequestContextWillSendSubsequentPayload } from '@apollo/server/dist/esm/externalTypes/requestPipeline'
-import { GraphQLContext, logGraphQLOperation } from '@makerx/graphql-core'
-import type { Logger } from '@makerx/node-common'
+import { GraphQLContext, logGraphQLOperation } from '@tuanbloom/graphql-core'
+import type { Logger } from '@tuanbloom/node-common'
 
 export interface GraphQLRequestInfo<TContext extends GraphQLContext<any, any, any>> {
   readonly requestContext: GraphQLRequestContextWillSendResponse<TContext>
